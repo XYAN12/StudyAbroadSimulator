387dd718fd9eb3283303e8661d4ae29a25a16352
XYAN12 <chemistry010612@gmail.com>
2024-11-01 01:20:37 +1100

    updated option buttons layout, tested choose destination

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("current-year").innerText = "准备留学！";
-    document.getElementById("month").innerText = "请选择您的留学目的地：";
+    document.getElementById("current-month").innerText = "请选择您的留学目的地：";
 
     showDestinationOptions();
 });
@@ -9,14 +9,11 @@ function showDestinationOptions() {
     const optionsDiv = document.getElementById("options");
     optionsDiv.innerHTML = "";
 
-    // Create buttons for Australia and Germany
     const australiaButton = document.createElement("button");
-    australiaButton.className = "btn btn-primary m-2";
     australiaButton.innerText = "澳大利亚";
     australiaButton.onclick = () => selectDestination("澳大利亚");
 
     const germanyButton = document.createElement("button");
-    germanyButton.className = "btn btn-primary m-2";
     germanyButton.innerText = "德国";
     germanyButton.onclick = () => selectDestination("德国");
 
@@ -26,8 +23,8 @@ function showDestinationOptions() {
 }
 
 function selectDestination(destination) {
-    document.getElementById("current-year").innerText = ""; // Clear the current year
-    document.getElementById("month").innerText = ""; // Clear the month
+    document.getElementById("current-year").innerText = "";
+    document.getElementById("month").innerText = "";
 
     const optionsDiv = document.getElementById("options");
     optionsDiv.innerHTML = `<p>您选择的目的地是：${destination}</p>`;
